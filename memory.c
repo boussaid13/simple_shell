4241177465b0206f04653d2b889d36be07def1f6
IBSD0010 <ibsd0010@gmail.com>
2023-07-25 17:01:13 -0700

    memory.c

---
+++ memory.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * bfree - frees a pointer and NULLs the adress
+ * @ptr: address of the pointer to free
+ * Return: 1 if freed, 0 otherwise
+ */
+int bfree(void **ptr)
+{
+if (ptr && *ptr)
+{
+free(*ptr);
+*ptr = NULL;
+return (1);
+}
+return (0);
+}
